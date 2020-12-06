675e5d56380e5dc2cc8e647ce09b93f2a7b453d9
MBAFST <75304576+MBAFST@users.noreply.github.com>
2020-12-06 14:47:15 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,35 +1,3 @@
-# import random
-#
-# print("H A N G M A N")
-#
-# words = ["python", "java", "kotlin", "javascript"]
-# answer = random.choice(words)
-#
-# tries = 8
-# guessed = set()
-# guess = ""
-# answer_wordset = set(answer)
-#
-# while tries > 0 and not answer_wordset.issubset(guessed):
-#     print()
-#
-#     print("".join([i if i in guessed else "-" for i in answer]))
-#     guess = str(input("Input a letter: "))
-#
-#     if len(guess) != 1:
-#         print("You should input a single letter")
-#     elif guess in guessed:
-#         print("You've already guessed this letter")
-#     elif not guess.islower():
-#         print("Please enter a lowercase English letter")
-#     elif guess not in answer_wordset and guess not in guessed:
-#         print("That letter doesn't appear in the word")
-#         tries -= 1
-#
-#     guessed.add(guess)
-#
-# print("You lost!" if not answer_wordset.issubset(guessed) else "You guessed the word " + answer + "!\nYou survived!")
-
 import random
 
 print("H A N G M A N")
